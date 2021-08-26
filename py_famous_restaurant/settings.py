2ea78d600a5215eaa66a48f2a941ff871ba10603
jojo3420 <jjjhhhvvv@gmail.com>
2021-08-26 15:41:39 +0900

    메일 전송 기능 구현 완료  - gmail.smtp

--- py_famous_restaurant/settings.py
+++ py_famous_restaurant/settings.py
@@ -125,3 +125,15 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email send setting
+with open('.env') as stream:
+    from_email, pwd = stream.readlines()
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = from_email.strip()
+    EMAIL_HOST_PASSWORD = pwd.strip()
+
--- sendEmail/views.py
+++ sendEmail/views.py
@@ -1,7 +1,40 @@
+import traceback
+
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, QueryDict
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from shareRes.models import Restaurant
 
 
 # Create your views here.
+
+
+def read_file(file_path):
+    with open(file_path) as stream:
+        lines = stream.readlines()
+        if len(lines) == 2:
+            return lines[0].strip(), lines[1].strip()
+
+
 def send(request):
-    return HttpResponse('send email')
+    restaurants = []
+    query_dict = QueryDict(request.body)
+    try:
+        receivers = query_dict['receivers'].split(',')
+        from_email, _pwd = read_file('.env')
+        for restaurant_id in query_dict['checked_restaurants']:
+            restaurants.append(Restaurant.objects.get(id=restaurant_id))
+        data = {
+            'restaurants': restaurants,
+            'content': query_dict['content'],
+        }
+        html_str = render_to_string('sendEmail/email_format.html', data)
+        email_message = EmailMessage(subject=query_dict['title'], body=html_str, from_email=from_email, bcc=receivers)
+        email_message.content_subtype = 'html'  # content_subtype 설정하지 않으면 메일 확인시 html 인식X 문자열로 인식함!
+        email_message.send()
+        return render(request, 'sendEmail/success.html')
+    except Exception as e:
+        print(str(e))
+        traceback.print_exc()
+        return render(request, 'sendEmail/failed.html')
